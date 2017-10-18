827bb6c3f79c4a0ec8b8df6ba115be2c3471981d
Anubiella <rathvelcar@libero.it>
2017-10-18 11:33:28 +0200

    Create carousel effect into jumbotron

---
+++ resources/js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  
+	$('.carousel').slick({
+		accessibility: false,
+		arrows: false,
+		draggable: false,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+	});
+});
+	
