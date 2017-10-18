29e02092a598a9539a75a51e4645ae9ee5f3b69b
Anubiella <rathvelcar@libero.it>
2017-10-18 12:04:52 +0200

    Project completed

--- resources/js/main.js
+++ resources/js/main.js
@@ -12,5 +12,20 @@ $(document).ready(function(){
 		autoplay: true,
 		autoplaySpeed: 4000,
 	});
+
+	var userScroll = 0;
+	$(window).scroll(function(){
+		var y = $(this).scrollTop();
+		if ((y-userScroll)>50){
+			var navheight = $('.navbar').css('height');
+			$('.navbar').animate({
+				top: '-'+navheight}, 150);
+			userScroll=y;
+		} else if((userScroll-y)>50){
+			$('.navbar').animate({
+				top: 0}, 150);
+			userScroll=y;
+		}
+	});
 });
 	
